f263994af71fc92f2ddaf07ffe23b9c69d10436c
Victor Kuo <vck3000@gmail.com>
2021-08-28 15:23:31 +1000

    isValid checks for bounds and weight.

--- backend/arrangement.py
+++ backend/arrangement.py
@@ -3,6 +3,8 @@ from item import Item
 from bag import Bag
 from typing import List
 
+from functools import reduce
+
 
 class Arrangement:
     """An arrangement is a solution.
@@ -22,8 +24,20 @@ class Arrangement:
         self.locations = locations
 
     def is_valid(self):
-        for location in self.locations:
-            if location.x > self.bag.size.x:
+        # Check item bounds
+        for item, location in zip(self.items, self.locations):
+            if location.x + item.size.x > self.bag.size.x:
                 return False
+
+            if location.y + item.size.y > self.bag.size.y:
+                return False
+
+        # Add up all item weights and ensure not exceeding bag
+        net_mass = 0
+        for item in self.items:
+            net_mass += item.get_mass()
+
+        if net_mass > self.bag.mass_limit:
+            return False
 
         return True
--- backend/test_arrangement.py
+++ backend/test_arrangement.py
@@ -8,24 +8,43 @@ from arrangement import Arrangement
 
 class TestArrangement(unittest.TestCase):
     def setUp(self):
-        self.bag = Bag(Coord(5, 5), 10)
+        self.bag = Bag(Coord(5, 5), 100)
         self.items = [Item(Coord(2, 3), 5, 10)]
 
-    def test_create_arrangement(self):
-        locations = [Coord(0, 0)]
+    def create_simple_arrangement(self, itemSize: Coord, location: Coord):
+        return Arrangement(self.bag, [Item(itemSize, 5, 10)], [location])
 
-        arrangement = Arrangement(self.bag, self.items, locations)
+    def test_create_arrangement(self):
+        self.create_simple_arrangement(Coord(2, 3), Coord(0, 0))
 
     def test_valid_arrangement(self):
-        locations = [Coord(0, 0)]
-
-        arrangement = Arrangement(self.bag, self.items, locations)
-
+        arrangement = self.create_simple_arrangement(Coord(2, 3), Coord(0, 0))
         self.assertEquals(True, arrangement.is_valid())
 
-    def test_invalid_arrangement(self):
-        locations = [Coord(10, 0)]
+    def test_invalid_arrangement_x_out_of_bound(self):
+        # Item way outside
+        arrangement = self.create_simple_arrangement(Coord(2, 3), Coord(10, 0))
+        self.assertEquals(False, arrangement.is_valid())
 
-        arrangement = Arrangement(self.bag, self.items, locations)
+        # Item over the edge of bag
+        arrangement = self.create_simple_arrangement(Coord(2, 3), Coord(4, 0))
+        self.assertEquals(False, arrangement.is_valid())
 
+        # Item on the edge of bag
+        arrangement = self.create_simple_arrangement(Coord(2, 3), Coord(3, 0))
+        self.assertEquals(True, arrangement.is_valid())
+
+    def test_too_heavy(self):
+        # 10kg bag is been given 30kg item
+        bag = Bag(Coord(5, 5), 10)
+        items = [Item(Coord(2, 3), 5, 10)]
+
+        arrangement = Arrangement(bag, items, [Coord(0, 0)])
+        self.assertEquals(False, arrangement.is_valid())
+
+    def test_item_overlap(self):
+        bag = Bag(Coord(5, 5), 10)
+        items = [Item(Coord(2, 3), 5, 10), Item(Coord(2, 3), 5, 10)]
+
+        arrangement = Arrangement(bag, items, [Coord(0, 0), Coord(0, 0)])
         self.assertEquals(False, arrangement.is_valid())
